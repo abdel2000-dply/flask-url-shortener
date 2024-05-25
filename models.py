308898255dce13e5d8bfcb0157219c0591ea4f4c
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-05-25 15:09:21 +0100

    Add URL model to models.py

---
+++ models.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""models.py
+        - This module contains the URL class
+"""
+from app import db
+
+
+class URL(db.Model):
+    """URL class
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    original_url = db.Column(db.String(500), nullable=False)
+    short_url = db.Column(db.String(100), unique=True, nullable=False)
+    visits = db.Column(db.Integer, default=0)
+
+    def __repr__(self):
+        """__repr__ method"""
+        return f"URL('{self.original_url}', '{self.short_url}', '{self.visits}')"
