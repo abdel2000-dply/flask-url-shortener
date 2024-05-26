cf76232fecf5fd2cdf803accdb61d8d98976971e
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-05-26 14:29:44 +0100

    chore: Update app.py to include a docstring and comments

--- app/app.py
+++ app/app.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+""" app.py 
+    - The main entry point for the application.
+    - This script runs the Flask application.
+"""
 from . import create_app
 
 app = create_app()
